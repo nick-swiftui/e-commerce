450b638bde5a0dedba2135c183bc767ed0292d7b
Nickson <nicktakesmail@gmail.com>
2024-12-09 13:44:13 -0500

    Add files via upload

---
+++ E-Commerce/E-Commerce/E_CommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceApp.swift
+//  E-Commerce
+//
+//  Created by Nicolas Deleasa on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
